450660bdea06c21b07956d3d5edbcb99c739411e
cervantesemiliano <cervantesemilianocervantes@gmail.com>
2026-02-11 20:37:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ html = """
 </html>
 """
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"]
 def index():
     if "historial" not in session:
         session["historial"] = []
